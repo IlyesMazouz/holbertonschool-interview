897faafb3974c3c31c0cf58e2f7619e6106208f7
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-01-14 12:05:24 +0100

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -5,13 +5,20 @@ module for canUnlockAll method
 
 from collections import deque
 
+
 def canUnlockAll(boxes):
     """
     determines if all boxes can be opened
+    args:
+        boxes (list): A list of lists, where each list represents a box and
+                      contains keys to other boxes
+
+    returns:
+        bool: True if all boxes can be opened, else False
     """
     n = len(boxes)
     visited = set()
-    queue = deque([0])  
+    queue = deque([0])
 
     while queue:
         current_box = queue.popleft()
@@ -23,12 +30,13 @@ def canUnlockAll(boxes):
 
     return len(visited) == n
 
+
 if __name__ == "__main__":
     boxes1 = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes1))  
+    print(canUnlockAll(boxes1))
 
     boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes2))  
+    print(canUnlockAll(boxes2))
 
     boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes3))  
+    print(canUnlockAll(boxes3))
