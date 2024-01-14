b37885e66d687fcce19abf55322310c1d19714c2
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-01-14 12:13:37 +0100

    L checker

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -4,24 +4,14 @@ Module for canUnlockAll method
 """
 
 from collections import deque
-"""
-DOCUMENTATION TESTING
-"""
 
 def canUnlockAll(boxes):
     """
-    Determines if all boxes can be opened.
-
-    Args:
-        boxes (list): A list of lists, where each list represents a box and
-                      contains keys to other boxes.
-
-    Returns:
-        bool: True if all boxes can be opened, else False.
+    Determines if all boxes can be opened
     """
     n = len(boxes)
     visited = set()
-    queue = deque([0])
+    queue = deque([0])  
 
     while queue:
         current_box = queue.popleft()
@@ -33,16 +23,12 @@ def canUnlockAll(boxes):
 
     return len(visited) == n
 
-
 if __name__ == "__main__":
-    """
-    test case
-    """
     boxes1 = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes1))
+    print(canUnlockAll(boxes1))  
 
     boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes2))
+    print(canUnlockAll(boxes2))  
 
     boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes3))
+    print(canUnlockAll(boxes3))  
