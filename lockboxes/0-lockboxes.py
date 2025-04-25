5c0c17b039234fdbecba8a5f0d2719bc7a07dce3
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-25 13:40:33 +0200

    pycodestyle fix

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -6,9 +6,10 @@ Determines if all boxes can be opened
 def canUnlockAll(boxes):
     """
     Checks whether all boxes can be unlocked starting from box 0
-
     """
-    if not isinstance(boxes, list) or not all(isinstance(box, list) for box in boxes):
+    if not isinstance(boxes, list):
+        return False
+    if not all(isinstance(box, list) for box in boxes):
         return False
 
     n = len(boxes)
