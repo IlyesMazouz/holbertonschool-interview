09bc8d3e5a461dedf0c35219a2ffb2951f77c3f1
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-26 18:56:20 +0200

    adding more documentation

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-"""Log parsing script"""
+""" 
+Script that reads stdin line by line and computes metrics
+"""
 
 import sys
 
 status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0,
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0,
 }
+
 total_size = 0
 line_count = 0
 
-
 def print_stats():
-    """Prints the accumulated statistics"""
+    """
+    Prints the accumulated statistics
+    """
     print("File size: {}".format(total_size))
     for code in sorted(status_codes.keys()):
         if status_codes[code] > 0:
             print("{}: {}".format(code, status_codes[code]))
-
 
 try:
     for line in sys.stdin:
@@ -31,16 +34,22 @@ try:
         if len(parts) >= 7:
             file_size = parts[-1]
             status_code = parts[-2]
+
             try:
                 total_size += int(file_size)
             except Exception:
                 pass
+
             if status_code in status_codes:
                 status_codes[status_code] += 1
+
         line_count += 1
+
         if line_count % 10 == 0:
             print_stats()
+
 except KeyboardInterrupt:
     print_stats()
     raise
+
 print_stats()
