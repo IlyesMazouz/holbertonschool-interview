0222a155a1362e5bae362f05fd93e5ff538d9f9c
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-26 18:53:56 +0200

    0-stats.py file

---
+++ log_parsing/0-stats.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Log parsing script"""
+
+import sys
+
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
+total_size = 0
+line_count = 0
+
+
+def print_stats():
+    """Prints the accumulated statistics"""
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
+
+try:
+    for line in sys.stdin:
+        parts = line.split()
+        if len(parts) >= 7:
+            file_size = parts[-1]
+            status_code = parts[-2]
+            try:
+                total_size += int(file_size)
+            except Exception:
+                pass
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        line_count += 1
+        if line_count % 10 == 0:
+            print_stats()
+except KeyboardInterrupt:
+    print_stats()
+    raise
+print_stats()
