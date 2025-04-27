5cdd928b75c847eda38a547fecaea80432cd17b0
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-27 21:44:14 +0200

    pycodestyle fix

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -7,7 +7,9 @@ import sys
 def is_safe(board, row, col, n):
     """Check if it's safe to place a queen at board[row][col]"""
     for i in range(row):
-        if board[i] == col or board[i] - i == col - row or board[i] + i == col + row:
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
             return False
     return True
 
