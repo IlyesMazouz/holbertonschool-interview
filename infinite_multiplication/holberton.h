518e85bb657d8bd0a1183add3e5a1af5385d0e53
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-29 17:46:03 +0200

    header file

---
+++ infinite_multiplication/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Function prototype for _putchar */
+int _putchar(char c);
+
+/* Function prototype for checking if a string is a digit */
+int is_digit(char *str);
+
+#endif /* HOLBERTON_H */
