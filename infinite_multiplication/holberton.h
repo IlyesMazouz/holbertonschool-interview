487e0af66c5b6f5842babf67ed23726edbbf1603
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-29 17:50:07 +0200

    betty fix

--- infinite_multiplication/holberton.h
+++ infinite_multiplication/holberton.h
@@ -4,10 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* Function prototype for _putchar */
 int _putchar(char c);
 
-/* Function prototype for checking if a string is a digit */
 int is_digit(char *str);
 
 #endif /* HOLBERTON_H */
