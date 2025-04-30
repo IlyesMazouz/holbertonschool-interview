8e0d4e0769c5d59ba2c72a21d5ce35292e6749d9
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-30 07:49:41 +0200

    0-island_perimeter.py file

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Returns the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of an island in a grid.
+    """
+    if not grid or not grid[0]:
+        return 0
+
+    height = len(grid)
+    width = len(grid[0])
+    perimeter = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
